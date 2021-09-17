3af6dc78a48978ca868b33c64d9469e1f4795b2a
kimjiwon <76871198+hallu0317@users.noreply.github.com>
2021-09-17 23:19:18 +0900

    Create 0917.py

---
+++ 0917.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = 0
+    for num in arr:
+        answer += num
+    return answer/len(arr)
+  
+  
+def solution(arr):
+    answer = 0
+    answer = sum(arr) / len(arr)
+    return answer
