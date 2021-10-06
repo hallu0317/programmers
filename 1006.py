db7ccfd4d4754daa681fb336a5bb17f153f4a472
kimjiwon <76871198+hallu0317@users.noreply.github.com>
2021-10-06 23:29:46 +0900

    Update 1006.py

--- 1006.py
+++ 1006.py
@@ -9,3 +9,21 @@ def solution(s):
                 answer += answer2[i][j].lower()
         answer += ' '
     return answer[:-1]
+
+
+
+//
+def solution(s):
+    answer = ''
+    cnt = 0
+    for i in range(len(s)):
+        if(s[i]==' '):
+            cnt=0
+            answer += " "
+        else:
+            if(cnt%2==0):
+                answer += s[i].upper()
+            else:
+                answer += s[i].lower()
+            cnt=cnt+1
+    return answer
