c21dbcfa1495c005a34265c07b77cf681aeb18f8
kimjiwon <76871198+hallu0317@users.noreply.github.com>
2021-10-07 23:50:20 +0900

    Update 1007.py

--- 1007.py
+++ 1007.py
@@ -3,6 +3,18 @@ def solution(n):
     for i in range(1,n+1):
         if(n%i==0):
             answer += i
-        else:
-            continue
     return answer
+
+
+
+//js
+function solution(n) {
+    var answer = 0;
+    
+    for(var i=1;i<=n;i++) {
+        if(n%i==0) {
+            answer += i;
+        }
+    }
+    return answer;
+}
